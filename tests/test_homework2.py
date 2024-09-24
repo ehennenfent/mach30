a5585651af5268bdf359ee7d9e3a565563f9630d
Eric Hennenfent <ecapstone@gmail.com>
2024-09-24 02:13:15 +0000

    Update homework 2

--- tests/test_homework2.py
+++ tests/test_homework2.py
@@ -1,48 +1,48 @@
-from mach30 import ProgramBuilder
-from mach30.gcode import DrillCycle, RapidMove, SetWorkOffset, SpotDrillCycle
-from mach30.helpers import standard_preamble, use_tool
-from mach30.mcode import EndProgram, SpindleOn
-from mach30.models import SpindleDirection, WorkOffset
+from mach30.builder import ProgramBuilder
+from mach30.enums import WorkOffset
+from mach30.gcode import DrillCycle, SpotDrillCycle
+from mach30.models import SpindleDirection, SpindleSettings, Tool
 
 
 def test_build_homework():
+    t6 = Tool(
+        number=6, description="spot drill", spindle=SpindleSettings(direction=SpindleDirection.FORWARD, speed=2750)
+    )
+
+    t7 = Tool(
+        number=7, description="regular drill", spindle=SpindleSettings(direction=SpindleDirection.FORWARD, speed=4500)
+    )
+
     builder = ProgramBuilder(number=3, preamble_comments=["ehennenfent 9 holes"])
-    standard_preamble(builder)
+    with builder.program():
+        builder.default_config()
+        builder.set_work_offset(WorkOffset.ONE)
 
-    with SetWorkOffset(builder=builder, work_offset=WorkOffset.ONE):
-        with (
-            use_tool(builder=builder, tool_number=6),
-            SpindleOn(builder=builder, direction=SpindleDirection.FORWARD, speed=2750),
-        ):
-            with RapidMove(builder=builder) as rapid:
-                rapid.move(x=0, y=0, z=0.1)
-            with SpotDrillCycle(builder=builder, f=11, z=-0.150, r=0.1) as drill:
-                drill.move(x=0, y=1)
-                drill.move(x=-1.0, y=1)
-                drill.move(x=-1.0, y=0)
-                drill.move(x=-1.0, y=-1)
-                drill.move(x=0, y=-1)
-                drill.move(x=1.0, y=-1)
-                drill.move(x=1.0, y=0)
-                drill.move(x=1.0, y=1)
+        builder.rapid(x=0, y=0)
+        builder.use_tool(t6, final_z=0.1)
 
-        with (
-            use_tool(builder=builder, tool_number=7),
-            SpindleOn(builder=builder, direction=SpindleDirection.FORWARD, speed=4500),
-        ):
-            with RapidMove(builder=builder) as rapid:
-                rapid.move(x=0, y=0, z=0.1)
-            with DrillCycle(builder=builder, f=15, z=-0.350, r=0.1) as drill:
-                drill.move(x=0, y=1)
-                drill.move(x=-1.0, y=1)
-                drill.move(x=-1.0, y=0)
-                drill.move(x=-1.0, y=-1)
-                drill.move(x=0, y=-1)
-                drill.move(x=1.0, y=-1)
-                drill.move(x=1.0, y=0)
-                drill.move(x=1.0, y=1)
-    builder.add(EndProgram(comment="all done"))
+        with SpotDrillCycle(builder=builder, f=11, z=-0.150, r=0.1) as drill:
+            drill.move(x=0, y=1)
+            drill.move(x=-1.0, y=1)
+            drill.move(x=-1.0, y=0)
+            drill.move(x=-1.0, y=-1)
+            drill.move(x=0, y=-1)
+            drill.move(x=1.0, y=-1)
+            drill.move(x=1.0, y=0)
+            drill.move(x=1.0, y=1)
+
+        builder.use_tool(t7, final_z=0.1)
+
+        with DrillCycle(builder=builder, f=15, z=-0.350, r=0.1) as drill:
+            drill.move(x=0, y=1)
+            drill.move(x=-1.0, y=1)
+            drill.move(x=-1.0, y=0)
+            drill.move(x=-1.0, y=-1)
+            drill.move(x=0, y=-1)
+            drill.move(x=1.0, y=-1)
+            drill.move(x=1.0, y=0)
+            drill.move(x=1.0, y=1)
 
     rendered = builder.render()
     assert rendered.startswith("%\nO00003\n(ehennenfent 9 holes)\n")
-    assert rendered.endswith("M30 (all done)\n%")
+    assert rendered.endswith("M30 (end program)\n%")
